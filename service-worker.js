a08b255b3fbbce9bff647e4ca48d334252ab19c0
sazdett-png <sazdett@gmail.com>
2026-09-23 20:26:18 -0600

    Update cache name to 'bendit-cache-v2'

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bendit-cache-v1';
+const CACHE_NAME = 'bendit-cache-v2';
 
 // 1. При первой загрузке сохраняем основные файлы в память телефона
 self.addEventListener('install', event => {
