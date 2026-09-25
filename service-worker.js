dae0817acfea74679f45bfdf721d12b73e14380b
sazdett-png <sazdett@gmail.com>
2026-09-24 19:55:27 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bendit-cache-v4';
+const CACHE_NAME = 'bendit-cache-v5';
 
 // 1. При первой загрузке сохраняем основные файлы в память телефона
 self.addEventListener('install', event => {
